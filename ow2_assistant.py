f220efc7deb8e93917f619e468703550a242bcfc
sasaxopajic <sasaxxxpajic@gmail.com>
2024-10-02 15:50:54 +0200

    Fix UI components

--- ow2_assistant.py
+++ ow2_assistant.py
@@ -8,13 +8,12 @@ import textwrap
 
 with st.sidebar:
     with st.form(key='my_form'):
-        youtube_url = st.sidebar.text_area(
+        youtube_url = st.text_area(
             label="What is the YouTube video URL?",
             max_chars=50
             )
-        query = st.sidebar.text_area(
+        query = st.text_area(
             label="Ask me about the video?",
-            max_chars=50,
             key="query"
             )
         submit_button = st.form_submit_button(label='Submit')
@@ -23,4 +22,4 @@ if query and youtube_url:
         db = lch.create_vector_database_from_yt_url(youtube_url)
         response = lch.get_response_from_query(db, query)
         st.subheader("Answer:")
-        st.write(textwrap.fill(response, width=85))
+        st.markdown(response)
