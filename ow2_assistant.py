6597b2eae4a5cf88febbd3b2bfb8eb48b35b2208
sasaxopajic <sasaxxxpajic@gmail.com>
2024-10-03 16:56:48 +0200

    Update design, change styles and assistan avatar

--- ow2_assistant.py
+++ ow2_assistant.py
@@ -1,8 +1,24 @@
 import streamlit as st
 import ow2_langchain as lch
 
-st.header("Overwatch 2 assistant")
-st.subheader("Ask any question about strategies that you want to know!")
+# Apply Poppins font to the whole app
+st.markdown(
+    """
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <style>
+    html * {
+        font-family: 'Poppins', sans-serif;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
+st.image("./assets/ow2_logo.png", use_column_width=True)
+
+st.markdown("<h1 style='text-align: center'>Type: 3C-HO - Overwatch 2 Assistant</h1>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center'>All strategies you want to know!</h4>", unsafe_allow_html=True)
 
 # Initialize chat history in session state
 if 'chat_history' not in st.session_state:
@@ -34,5 +50,11 @@ if submit_button and query and youtube_url:
 # Display chat history
 if st.session_state.chat_history:
     for chat in st.session_state.chat_history:
-        with st.chat_message(chat["role"]):
-            st.write(chat["content"])
+            if chat["role"] == "user":
+                with st.chat_message("user"):
+                    st.write(chat["content"])
+            else:
+                # Display the assistant's image and then the message content
+                with st.chat_message("assistant", avatar="./assets/ow2_icon.png"):
+                    st.markdown("<h4 style='padding: 0; margin-bottom: 1.5rem; font-weight: 900; color: #F99E1A'>Type: 3C-HO</h4>", unsafe_allow_html=True)
+                    st.write(chat["content"],)
